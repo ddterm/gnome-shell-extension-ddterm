0a612bc56e1f4f15bbfdc8e9d6676fc87fb19997
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-03-17 07:07:58 +0600

    ci: use image from external repo
    https://github.com/amezin/gnome-shell-extension-ddterm-ci-docker-image

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM fedora:33
-RUN dnf install -y gtk3-devel gtk4-devel libxslt gnome-shell make npm xorg-x11-server-Xvfb
-RUN npm install -g eslint
--- do-in-docker.sh
+++ do-in-docker.sh
@@ -2,13 +2,10 @@
 
 set -ex
 
-mkdir -p tmp
-DOCKER_BUILDKIT=1 docker build --iidfile tmp/docker.iid .
-
 TTY_FLAG=-t
 
 if [ ! -t 0 ]; then
     TTY_FLAG=
 fi
 
-exec docker run --rm -i $TTY_FLAG -u $(id -u):$(id -g) -v "${PWD}:${PWD}" -w "${PWD}" $(cat tmp/docker.iid) xvfb-run "$@"
+exec docker run --rm -i $TTY_FLAG -u $(id -u):$(id -g) -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-extension-ddterm-ci-docker-image:master "$@"
