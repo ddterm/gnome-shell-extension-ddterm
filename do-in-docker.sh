17d02013a285bac97d47b2c5428e8435bfb55f78
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-03-17 07:37:36 +0600

    do-in-docker.sh: add xvfb-run

--- do-in-docker.sh
+++ do-in-docker.sh
@@ -8,4 +8,4 @@ if [ ! -t 0 ]; then
     TTY_FLAG=
 fi
 
-exec docker run --rm -i $TTY_FLAG -u $(id -u):$(id -g) -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-extension-ddterm-ci-docker-image:master "$@"
+exec docker run --rm -i $TTY_FLAG -u $(id -u):$(id -g) -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-extension-ddterm-ci-docker-image:master xvfb-run "$@"
