3d6400a2d1e44937717103e5bfc3a008eda1dee7
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-05-27 03:04:28 +0600

    tests: increase timeout again

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec gdbus call --session --timeout 300 --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/ddterm --method com.github.amezin.ddterm.ExtensionTest.RunTest "$@"
+exec gdbus call --session --timeout 1000 --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/ddterm --method com.github.amezin.ddterm.ExtensionTest.RunTest "$@"
