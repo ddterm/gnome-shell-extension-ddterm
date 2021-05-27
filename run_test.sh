5cb4b58bf9da5be95b040690eb37c98a3a6091da
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-05-27 08:05:09 +0600

    Remove run_test.sh
    Nobody should probably use it (without a container environment).

--- run_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec gdbus call --session --timeout 1000 --dest org.gnome.Shell --object-path /org/gnome/Shell/Extensions/ddterm --method com.github.amezin.ddterm.ExtensionTest.RunTest "$@"
