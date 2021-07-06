06a58d3e4df5fe2ae91531973f7f75faee4b5ef5
Aleksandr Mezin <mezin.alexander@gmail.com>
2021-07-06 10:59:20 +0600

    ci: switch to gnome-shell-pod-* images completely

--- do-in-docker.sh
+++ do-in-docker.sh
@@ -8,4 +8,4 @@ if [ ! -t 0 ]; then
     TTY_FLAG=
 fi
 
-exec docker run --rm -i $TTY_FLAG -u $(id -u):$(id -g) -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-extension-ddterm-ci-docker-image:master xvfb-run "$@"
+exec docker run --rm -i $TTY_FLAG -u $(id -u):$(id -g) -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-pod-34:master xvfb-run "$@"
--- do-in-podman.sh
+++ do-in-podman.sh
@@ -8,4 +8,4 @@ if [ ! -t 0 ]; then
     TTY_FLAG=
 fi
 
-exec podman run --rm -i $TTY_FLAG -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-extension-ddterm-ci-docker-image:master xvfb-run "$@"
+exec podman run --rm -i $TTY_FLAG -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-pod-34:master xvfb-run "$@"
