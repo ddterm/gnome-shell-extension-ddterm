2bc24a2fb44030cfdf76353a8eb9c2e56eda28ff
Aleksandr Mezin <mezin.alexander@gmail.com>
2022-04-14 13:11:58 +0600

    Pass '--init' to podman run/docker run

--- do-in-docker.sh
+++ do-in-docker.sh
@@ -8,4 +8,4 @@ if [ ! -t 0 ]; then
     TTY_FLAG=
 fi
 
-exec docker run --rm -i $TTY_FLAG -u $(id -u):$(id -g) -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-pod-34:master xvfb-run "$@"
+exec docker run --init --rm -i $TTY_FLAG -u $(id -u):$(id -g) -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-pod-34:master xvfb-run "$@"
