ec7b5c365916b97b9cd0d0da045d98aac92aab4b
Aleksandr Mezin <mezin.alexander@gmail.com>
2022-07-07 09:13:29 +0600

    do-in-{docker,podman}.sh: always mount root of the repo

--- do-in-docker.sh
+++ do-in-docker.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(CDPATH="" cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 TTY_FLAG=$(test -t 0 && echo -n -t)
 UID_GID=$(id -u):$(id -g)
 
 set -ex
 
-exec docker run --init --rm -i $TTY_FLAG -u $UID_GID -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-extension-ddterm-ci-docker-image:master xvfb-run "$@"
+exec docker run --init --rm -i $TTY_FLAG -u $UID_GID -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" ghcr.io/amezin/gnome-shell-extension-ddterm-ci-docker-image:master xvfb-run "$@"
--- do-in-podman.sh
+++ do-in-podman.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(CDPATH="" cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 TTY_FLAG=$(test -t 0 && echo -n -t)
 
 set -ex
 
-exec podman run --init --rm -i $TTY_FLAG -v "${PWD}:${PWD}" -w "${PWD}" ghcr.io/amezin/gnome-shell-extension-ddterm-ci-docker-image:master xvfb-run "$@"
+exec podman run --init --rm -i $TTY_FLAG -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" ghcr.io/amezin/gnome-shell-extension-ddterm-ci-docker-image:master xvfb-run "$@"
