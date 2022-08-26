1144f104e20457c5dfe4896cb6458d74f3d0abba
Aleksandr Mezin <mezin.alexander@gmail.com>
2022-08-27 05:43:39 +0600

    test/dbus_util: interrupt Introspect() calls when name owner changes

--- test/dbus_util.py
+++ test/dbus_util.py
@@ -5,26 +5,24 @@ from gi.repository import GLib, Gio
 
 LOGGER = logging.getLogger(__name__)
 
-
-def call(connection, method, signature=None, *args, dest, path, interface, return_type=None, timeout=None):
-    return connection.call_sync(
-        dest,
-        path,
-        interface,
-        method,
-        GLib.Variant(signature, args) if signature or args else None,
-        GLib.VariantType.new(return_type) if return_type else None,
-        Gio.DBusCallFlags.NONE,
-        -1 if timeout is None else int(timeout * 1000),
-        None
-    ).unpack()
+INTROSPECTABLE_IFACE = Gio.DBusNodeInfo.new_for_xml('''
+    <!DOCTYPE node PUBLIC "-//freedesktop//DTD D-BUS Object Introspection 1.0//EN"
+        "http://www.freedesktop.org/standards/dbus/1.0/introspect.dtd">
+    <node>
+        <interface name="org.freedesktop.DBus.Introspectable">
+            <method name="Introspect">
+                <arg type="s" name="xml_data" direction="out"/>
+            </method>
+        </interface>
+    </node>
+''').interfaces[0]
 
 
 class OneShotTimer:
     def __init__(self):
         self.source = None
 
-    def cancel(self, *_):
+    def cancel(self):
         if self.source is not None:
             self.source.destroy()
             self.source = None
@@ -43,54 +41,100 @@ class OneShotTimer:
 
 
 def wait_interface(connection, name, path, interface):
+    introspectable = Gio.DBusProxy.new_sync(
+        connection,
+        Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES,
+        INTROSPECTABLE_IFACE,
+        name,
+        path,
+        INTROSPECTABLE_IFACE.name,
+        None
+    )
+
     loop = GLib.MainLoop.new(None, False)
     retry = OneShotTimer()
     interface_info = None
 
-    def introspect(connection, name, owner):
-        LOGGER.info('Calling Introspect() on destination %r, path %r', owner, path)
+    def introspect_result(source, result):
+        owner = introspectable.get_name_owner()
+
         try:
-            (data,) = call(connection, 'Introspect', return_type='(s)', dest=owner, path=path, interface='org.freedesktop.DBus.Introspectable')
-            LOGGER.info('Introspect() call succeeded on destination %r, path %r', owner, path)
-
-            nonlocal interface_info
+            (data,) = source.call_finish(result).unpack()
             node_info = Gio.DBusNodeInfo.new_for_xml(data)
-            interface_info = node_info.lookup_interface(interface)
-
-            if interface_info:
-                loop.quit()
-                return
-            else:
-                LOGGER.info('Interface %r not found in Introspect() results on destination %r, path %r', interface, owner, path)
 
         except GLib.Error:
-            LOGGER.exception('Introspect() call failed on destination %r, path %r', owner, path)
+            LOGGER.exception(
+                'Introspect() call failed on destination %r, path %r',
+                owner,
+                path
+            )
 
-        retry.schedule(50, lambda *_: introspect(connection, name, owner))
+            retry.schedule(50, introspect)
+            return
 
-    watch_id = Gio.bus_watch_name_on_connection(
-        connection,
-        name,
-        Gio.BusNameWatcherFlags.NONE,
-        introspect,
-        retry.cancel
-    )
+        nonlocal interface_info
+        interface_info = node_info.lookup_interface(interface)
+
+        LOGGER.info(
+            'Interface %r %s on destination %r, path %r',
+            interface,
+            'found' if interface_info else 'not found',
+            owner,
+            path
+        )
+
+        if interface_info:
+            loop.quit()
+        else:
+            retry.schedule(50, introspect)
+
+    cancellable = None
+
+    def introspect(*_):
+        retry.cancel()
+
+        nonlocal cancellable
+        if cancellable:
+            cancellable.cancel()
+
+        owner = introspectable.get_name_owner()
+        if not owner:
+            LOGGER.info('No name owner for %r', name)
+            return
+
+        LOGGER.info('Calling Introspect() on destination %r, path %r', owner, path)
+
+        cancellable = Gio.Cancellable()
+
+        introspectable.call(
+            'Introspect',
+            None,
+            Gio.DBusCallFlags.NONE,
+            -1,
+            cancellable,
+            introspect_result
+        )
+
+    watch_id = introspectable.connect('notify::g-name-owner', introspect)
 
     try:
-        loop.run()
+        introspect()
+
+        if not interface_info:
+            loop.run()
 
         return Gio.DBusProxy.new_sync(
             connection,
             Gio.DBusProxyFlags.GET_INVALIDATED_PROPERTIES,
             interface_info,
-            name,
+            introspectable.get_name_owner(),
             path,
             interface,
             None
         )
 
     finally:
-        Gio.bus_unwatch_name(watch_id)
+        introspectable.disconnect(watch_id)
         retry.cancel()
 
 
