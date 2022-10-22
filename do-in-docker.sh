5896791963f7fc6b68463155ee1d2e19278d0aa9
Aleksandr Mezin <mezin.alexander@gmail.com>
2022-10-23 00:15:40 +0600

    do-in-{docker,podman}.sh: store and update image version

--- do-in-docker.sh
+++ do-in-docker.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+IMAGE=ghcr.io/ddterm/ci-docker-image
+# renovate: datasource=docker depName=ghcr.io/ddterm/ci-docker-image
+IMAGE_VERSION=2022.09.12.1
+
 SCRIPT_DIR=$(CDPATH="" cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 TTY_FLAG=$(test -t 0 && echo -n -t)
@@ -7,4 +11,4 @@ UID_GID=$(id -u):$(id -g)
 
 set -ex
 
-exec docker run --init --rm -i $TTY_FLAG -u $UID_GID -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" ghcr.io/ddterm/ci-docker-image:latest xvfb-run "$@"
+exec docker run --init --rm -i $TTY_FLAG -u $UID_GID -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
--- do-in-podman.sh
+++ do-in-podman.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+IMAGE=ghcr.io/ddterm/ci-docker-image
+# renovate: datasource=docker depName=ghcr.io/ddterm/ci-docker-image
+IMAGE_VERSION=2022.09.12.1
+
 SCRIPT_DIR=$(CDPATH="" cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 TTY_FLAG=$(test -t 0 && echo -n -t)
 
 set -ex
 
-exec podman run --init --rm -i $TTY_FLAG -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" ghcr.io/ddterm/ci-docker-image:latest xvfb-run "$@"
+exec podman run --init --rm -i $TTY_FLAG -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
