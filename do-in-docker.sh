22dd489a030716c47989c41a2d7318ec6e6f5928
Aleksandr Mezin <mezin.alexander@gmail.com>
2022-11-11 19:00:41 +0600

    do-in-docker.sh: sudo if necessary

--- do-in-docker.sh
+++ do-in-docker.sh
@@ -9,6 +9,12 @@ SCRIPT_DIR=$(CDPATH="" cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null &&
 TTY_FLAG=$(test -t 0 && echo -n -t)
 UID_GID=$(id -u):$(id -g)
 
+if [[ " $(groups) " =~ " docker " ]]; then
+    docker_cmd=(docker)
+else
+    docker_cmd=(sudo -g docker docker)
+fi
+
 set -ex
 
-exec docker run --init --rm -i $TTY_FLAG -u $UID_GID -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
+exec "${docker_cmd[@]}" run --init --rm -i $TTY_FLAG -u $UID_GID -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
