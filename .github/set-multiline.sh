c2fe9385d1b210a899245c7bea379957031455e6
Aleksandr Mezin <mezin.alexander@gmail.com>
2023-04-21 02:12:45 +0300

    ci: fix pot workflow for long diffs
    Avoid passing diff content as argument or environment variable.

--- .github/set-multiline.sh
+++ .github/set-multiline.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 1 ] || [ $# -gt 2 ]; then
-    echo "Usage: $0 key value"
+if [ $# -lt 1 ] || [ $# -gt 1 ]; then
+    echo "Usage: $0 key"
     exit 1
 fi
 
 EOF="$(dd if=/dev/urandom bs=15 count=1 status=none | base64)"
 
 echo "$1<<$EOF"
-[ -z "$2" ] || echo "$2"
+cat -
 echo "$EOF"
