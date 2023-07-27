7370040e66e2063785e4b7eb0ee75354246807e5
Zerocool56 <sukov.e.v@gmail.com>
2023-07-27 16:46:05 +0400

    Fix do-in-docker and do-in-podman (#498)

--- do-in-docker.sh
+++ do-in-docker.sh
@@ -5,7 +5,7 @@ IMAGE=ghcr.io/ddterm/ci-docker-image
 IMAGE_VERSION=2023.07.04.0
 
 SCRIPT_DIR=$(CDPATH="" cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
+HOME_DIR="${SCRIPT_DIR}/.container-home"
 TTY_FLAG=$(test -t 0 && echo -n -t)
 UID_GID=$(id -u):$(id -g)
 
@@ -17,4 +17,4 @@ fi
 
 set -ex
 
-exec "${docker_cmd[@]}" run --init --rm -i $TTY_FLAG -u $UID_GID -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
+exec "${docker_cmd[@]}" run --init --rm -i $TTY_FLAG -u $UID_GID -e HOME="${HOME_DIR}" --security-opt label=disable -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
--- do-in-podman.sh
+++ do-in-podman.sh
@@ -5,9 +5,10 @@ IMAGE=ghcr.io/ddterm/ci-docker-image
 IMAGE_VERSION=2023.07.04.0
 
 SCRIPT_DIR=$(CDPATH="" cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+HOME_DIR="${SCRIPT_DIR}/.container-home"
 
 TTY_FLAG=$(test -t 0 && echo -n -t)
 
 set -ex
 
-exec podman run --init --rm -i $TTY_FLAG -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
+exec podman run --init --rm -i $TTY_FLAG -e HOME="${HOME_DIR}" --security-opt label=disable -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
