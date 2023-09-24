371062e6415ad389a4b31e085d6e96d7b3f4db8c
Aleksandr Mezin <mezin.alexander@gmail.com>
2023-09-24 23:50:48 +0300

    ci: add bump-version workflow

---
+++ .github/bump-version.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+command -V jq
+
+CURRENT_VERSION=$(jq .version metadata.json.in)
+NEXT_VERSION=$(( ${CURRENT_VERSION} + 1 ))
+
+git tag v${CURRENT_VERSION}
+
+jq ".version=${NEXT_VERSION}" metadata.json.in > metadata.json.next
+mv -f metadata.json.next metadata.json.in
+make metadata.json
+
+sed -i "/^pkgver=/c\pkgver=${NEXT_VERSION}" PKGBUILD
+
+git commit -m "[ci skip] Post-release version bump" metadata.json.in PKGBUILD
