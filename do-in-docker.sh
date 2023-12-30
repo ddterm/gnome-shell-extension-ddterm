934c794569762f3d7192926f2b796b164baceef7
Aleksandr Mezin <mezin.alexander@gmail.com>
2023-12-30 23:06:27 +0200

    do-in-{podman,docker}.sh: use `realpath`

--- do-in-docker.sh
+++ do-in-docker.sh
@@ -4,10 +4,11 @@ IMAGE=ghcr.io/ddterm/ci-docker-image
 # renovate: datasource=docker depName=ghcr.io/ddterm/ci-docker-image
 IMAGE_VERSION=2023.12.26.0
 
-SCRIPT_DIR=$(CDPATH="" cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+SCRIPT_REALPATH="$(realpath -- "$0")"
+SCRIPT_DIR="${SCRIPT_REALPATH%/*}"
 HOME_DIR="${SCRIPT_DIR}/.container-home"
-TTY_FLAG=$(test -t 0 && echo -n -t)
-UID_GID=$(id -u):$(id -g)
+TTY_FLAG="$(test -t 0 && echo -n -t)"
+UID_GID="$(id -u):$(id -g)"
 
 if [[ " $(groups) " =~ " docker " ]]; then
     docker_cmd=(docker)
@@ -17,4 +18,4 @@ fi
 
 set -ex
 
-exec "${docker_cmd[@]}" run --init --rm -i $TTY_FLAG -u $UID_GID -e HOME="${HOME_DIR}" --security-opt label=disable -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
+exec "${docker_cmd[@]}" run --init --rm -i "${TTY_FLAG}" -u "${UID_GID}" -e HOME="${HOME_DIR}" --security-opt label=disable -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
--- do-in-podman.sh
+++ do-in-podman.sh
@@ -4,11 +4,12 @@ IMAGE=ghcr.io/ddterm/ci-docker-image
 # renovate: datasource=docker depName=ghcr.io/ddterm/ci-docker-image
 IMAGE_VERSION=2023.12.26.0
 
-SCRIPT_DIR=$(CDPATH="" cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+SCRIPT_REALPATH="$(realpath -- "$0")"
+SCRIPT_DIR="${SCRIPT_REALPATH%/*}"
 HOME_DIR="${SCRIPT_DIR}/.container-home"
 
-TTY_FLAG=$(test -t 0 && echo -n -t)
+TTY_FLAG="$(test -t 0 && echo -n -t)"
 
 set -ex
 
-exec podman run --init --rm -i $TTY_FLAG -e HOME="${HOME_DIR}" --security-opt label=disable -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
+exec podman run --init --rm -i "${TTY_FLAG}" -e HOME="${HOME_DIR}" --security-opt label=disable -v "${SCRIPT_DIR}:${SCRIPT_DIR}" -w "${PWD}" "${IMAGE}:${IMAGE_VERSION}" xvfb-run "$@"
