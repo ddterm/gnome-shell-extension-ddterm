800eb97a83ccf8a1c85b3065d229ddc09315112a
Aleksandr Mezin <mezin.alexander@gmail.com>
2024-03-10 13:52:38 +0200

    meson: capture output using python script
    A bit less hacky way

---
+++ tools/capture-stdout.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import stat
+import sys
+
+
+def capture_stdout(output, argv):
+    mode = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH
+    fd = os.open(output, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, mode)
+    os.dup2(fd, sys.stdout.fileno())
+    os.execvp(argv[0], argv)
+
+
+def cli(*args, **kwargs):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', required=True)
+    parser.add_argument('argv', nargs='+')
+
+    capture_stdout(**vars(parser.parse_args(*args, **kwargs)))
+
+
+if __name__ == '__main__':
+    cli()
