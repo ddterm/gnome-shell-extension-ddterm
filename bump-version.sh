9a9f60314174f72940f37bc983fc3f084826ea8c
Aleksandr Mezin <mezin.alexander@gmail.com>
2024-11-16 21:17:31 +0200

    ci: don't bump version automatically after tagging
    Fixes https://github.com/ddterm/gnome-shell-extension-ddterm/issues/958

--- bump-version.sh
+++ bump-version.sh
@@ -10,4 +10,5 @@ NEXT_VERSION=$(( ${CURRENT_VERSION} + 1 ))
 meson rewrite kwargs set project / version "${NEXT_VERSION}"
 sed -i "/^pkgver=/c\pkgver=${NEXT_VERSION}" PKGBUILD
 
-git add meson.build PKGBUILD
+git commit -m "Release v${NEXT_VERSION}" meson.build PKGBUILD
+git tag "v${NEXT_VERSION}"
