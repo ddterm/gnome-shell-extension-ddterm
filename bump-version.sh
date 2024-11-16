f4a10ab505ae4ff9a85d6e789efb5fa05473c1c1
Aleksandr Mezin <mezin.alexander@gmail.com>
2024-11-16 21:36:56 +0200

    meson: fix formatting

--- bump-version.sh
+++ bump-version.sh
@@ -9,6 +9,7 @@ NEXT_VERSION=$(( ${CURRENT_VERSION} + 1 ))
 
 meson rewrite kwargs set project / version "${NEXT_VERSION}"
 sed -i "/^pkgver=/c\pkgver=${NEXT_VERSION}" PKGBUILD
+meson format -e -i meson.build
 
 git commit -m "Release v${NEXT_VERSION}" meson.build PKGBUILD
 git tag "v${NEXT_VERSION}"
