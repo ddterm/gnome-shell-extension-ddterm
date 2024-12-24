8a06ac607e01b4c98ef79c9e30f6b0e45d7f7c48
Aleksandr Mezin <mezin.alexander@gmail.com>
2024-12-24 23:05:42 +0200

    tools/makezip.py: support multiple base directories
    Allow inclusion of files without copying to the build dir

--- tools/makezip.py
+++ tools/makezip.py
@@ -5,11 +5,31 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import argparse
-import os.path
+import pathlib
 import zipfile
 
 
-def makezip(output, inputs, relative_to):
+def find_relative_path(path, relative_to):
+    path = path.absolute()
+
+    if not relative_to:
+        relative_to = [pathlib.Path.cwd()]
+
+    for basepath in relative_to:
+        basepath = basepath.absolute()
+
+        try:
+            return path.relative_to(basepath).as_posix()
+        except ValueError as ex:
+            pass
+
+    raise ValueError(f'{str(path)!r} is not a subpath of any of {[str(p) for p in relative_to]}')
+
+
+def makezip(output, inputs, relative_to=None):
+    if not relative_to:
+        relative_to = [pathlib.Path.cwd()]
+
     with zipfile.ZipFile(
         output,
         mode='w',
@@ -17,14 +37,14 @@ def makezip(output, inputs, relative_to):
         compresslevel=9,
     ) as out:
         for infile in inputs:
-            out.write(infile, arcname=os.path.relpath(infile, relative_to))
+            out.write(infile, arcname=find_relative_path(infile, relative_to))
 
 
 def cli(*args, **kwargs):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--relative-to', default=os.curdir)
-    parser.add_argument('--output', required=True)
-    parser.add_argument('inputs', nargs='+')
+    parser.add_argument('--relative-to', type=pathlib.Path, default=[], action='append')
+    parser.add_argument('--output', type=pathlib.Path, required=True)
+    parser.add_argument('inputs', type=pathlib.Path, nargs='+')
 
     makezip(**vars(parser.parse_args(*args, **kwargs)))
 
