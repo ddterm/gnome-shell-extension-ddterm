c26b17e492980ecf46aa6c7290747dd13a6a8c22
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-01-27 05:50:53 +0200

    pref/shortcuts: fix memory leaks on Gtk 4

--- ddterm/pref/shortcuts.js
+++ ddterm/pref/shortcuts.js
@@ -22,6 +22,61 @@ const COLUMN_ACCEL_KEY = 2;
 const COLUMN_ACCEL_MODS = 3;
 const COLUMN_EDITABLE = 4;
 
+function update_model(model, iter, settings, key) {
+    const strv = settings.get_strv(key);
+    const [accel_key, accel_mods] =
+        strv.length > 0 ? accelerator_parse(strv[0]) : [0, 0];
+
+    model.set(
+        iter,
+        [COLUMN_ACCEL_KEY, COLUMN_ACCEL_MODS],
+        [accel_key, accel_mods]
+    );
+}
+
+function update_editable(model, iter, settings, key) {
+    model.set_value(iter, COLUMN_EDITABLE, settings.is_writable(key));
+}
+
+function save_shortcut(settings, model, renderer, path, accel_key = null, accel_mods = null) {
+    const [ok, iter] = model.get_iter_from_string(path);
+    if (!ok)
+        return;
+
+    settings.set_strv(
+        model.get_value(iter, COLUMN_SETTINGS_KEY),
+        accel_key ? [Gtk.accelerator_name(accel_key, accel_mods)] : []
+    );
+}
+
+function reset(settings, model) {
+    // eslint-disable-next-line no-shadow
+    model.foreach((model, path, iter) => {
+        const key = model.get_value(iter, COLUMN_SETTINGS_KEY);
+        settings.reset(key);
+    });
+}
+
+function remove_shortcut(settings, model, renderer, path, accel_key = null, accel_mods = null) {
+    if (!accel_key && !accel_mods)
+        return;
+
+    // eslint-disable-next-line no-shadow
+    model.foreach((model, path, iter) => {
+        const settings_key = model.get_value(iter, COLUMN_SETTINGS_KEY);
+        const value = settings.get_strv(settings_key);
+        const index = value.findIndex(accel => {
+            const [key, mods] = accelerator_parse(accel);
+            return key === accel_key && mods === accel_mods;
+        });
+
+        if (index !== -1) {
+            value.splice(index, 1);
+            settings.set_strv(settings_key, value);
+        }
+    });
+}
+
 export const ShortcutsWidget = GObject.registerClass({
     GTypeName: 'DDTermPrefsShortcuts',
     Template: ui_file_uri('prefs-shortcuts.ui'),
@@ -55,123 +110,97 @@ export const ShortcutsWidget = GObject.registerClass({
 
         insert_settings_actions(this, this.settings, ['shortcuts-enabled']);
 
-        [this.shortcuts_list, this.global_shortcuts_list].forEach(shortcuts_list => {
-            shortcuts_list.foreach((model, path, iter) => {
-                const i = iter.copy();
-                const key = model.get_value(i, COLUMN_SETTINGS_KEY);
-
-                const handler = this.settings.connect(
-                    `changed::${key}`,
-                    this.update_model.bind(this, model, i)
+        for (const renderer of [this.accel_renderer, this.global_accel_renderer]) {
+            for (const model of [this.shortcuts_list, this.global_shortcuts_list]) {
+                renderer.connect(
+                    'accel-edited',
+                    remove_shortcut.bind(globalThis, this.settings, model)
                 );
-                this.connect('destroy', () => this.settings.disconnect(handler));
-                this.update_model(model, i, this.settings, key);
-
-                const editable_handler = this.settings.connect(
-                    `writable-changed::${key}`,
-                    this.update_editable.bind(this, model, i)
-                );
-                this.connect('destroy', () => this.settings.disconnect(editable_handler));
-                this.update_editable(model, i, this.settings, key);
-
-                return false;
-            });
-        });
+            }
+        }
 
         for (const signal of ['accel-edited', 'accel-cleared']) {
             this.accel_renderer.connect(
                 signal,
-                this.save_shortcut.bind(this, this.shortcuts_list)
+                save_shortcut.bind(globalThis, this.settings, this.shortcuts_list)
             );
 
             this.global_accel_renderer.connect(
                 signal,
-                this.save_shortcut.bind(this, this.global_shortcuts_list)
+                save_shortcut.bind(globalThis, this.settings, this.global_shortcuts_list)
             );
         }
 
-        this.global_accel_renderer.connect(
-            'editing-started',
-            (IS_GTK3 ? this.grab_global_keys : this.inhibit_system_shortcuts).bind(this)
-        );
-
         bind_sensitive(this.settings, 'shortcuts-enabled', this.shortcuts_treeview);
 
-        this.accel_toggle.connect('toggled', (_, path) => {
-            this.save_shortcut(this.shortcuts_list, _, path);
-        });
+        this.accel_toggle.connect(
+            'toggled',
+            save_shortcut.bind(globalThis, this.settings, this.shortcuts_list)
+        );
 
-        this.global_accel_toggle.connect('toggled', (_, path) => {
-            this.save_shortcut(this.global_shortcuts_list, _, path);
-        });
+        this.global_accel_toggle.connect(
+            'toggled',
+            save_shortcut.bind(globalThis, this.settings, this.global_shortcuts_list)
+        );
 
         const reset_action = new Gio.SimpleAction({ name: 'reset' });
-        reset_action.connect('activate', this.reset.bind(this));
+
+        reset_action.connect(
+            'activate',
+            reset.bind(globalThis, this.settings, this.shortcuts_list)
+        );
+
+        reset_action.connect(
+            'activate',
+            reset.bind(globalThis, this.settings, this.global_shortcuts_list)
+        );
 
         const aux_actions = new Gio.SimpleActionGroup();
         aux_actions.add_action(reset_action);
         this.insert_action_group('aux', aux_actions);
+
+        this.connect('realize', () => {
+            const settings_handlers = [];
+            const inhibit_shortcuts_handler = this.global_accel_renderer.connect(
+                'editing-started',
+                (IS_GTK3 ? this.grab_global_keys : this.inhibit_system_shortcuts).bind(this)
+            );
+
+            for (const shortcuts_list of [this.shortcuts_list, this.global_shortcuts_list]) {
+                shortcuts_list.foreach((model, path, iter) => {
+                    const i = iter.copy();
+                    const key = model.get_value(i, COLUMN_SETTINGS_KEY);
+
+                    settings_handlers.push(
+                        this.settings.connect(
+                            `changed::${key}`,
+                            update_model.bind(globalThis, model, i)
+                        ),
+                        this.settings.connect(
+                            `writable-changed::${key}`,
+                            update_editable.bind(globalThis, model, i)
+                        )
+                    );
+
+                    update_model(model, i, this.settings, key);
+                    update_editable(model, i, this.settings, key);
+
+                    return false;
+                });
+            }
+
+            const unrealize_handler = this.connect('unrealize', () => {
+                this.disconnect(unrealize_handler);
+                this.global_accel_renderer.disconnect(inhibit_shortcuts_handler);
+
+                for (const handler of settings_handlers)
+                    this.settings.disconnect(handler);
+            });
+        });
     }
 
     get title() {
         return this.gettext_context.gettext('Keyboard Shortcuts');
-    }
-
-    update_model(model, iter, settings, key) {
-        const strv = settings.get_strv(key);
-        const [accel_key, accel_mods] =
-            strv.length > 0 ? accelerator_parse(strv[0]) : [0, 0];
-
-        model.set(
-            iter,
-            [COLUMN_ACCEL_KEY, COLUMN_ACCEL_MODS],
-            [accel_key, accel_mods]
-        );
-    }
-
-    update_editable(model, iter, settings, key) {
-        model.set_value(iter, COLUMN_EDITABLE, settings.is_writable(key));
-    }
-
-    save_shortcut(shortcuts_list, _, path, accel_key = null, accel_mods = null) {
-        if (accel_key) {
-            this.remove_shortcut(this.shortcuts_list, accel_key, accel_mods);
-            this.remove_shortcut(this.global_shortcuts_list, accel_key, accel_mods);
-        }
-
-        const [ok, iter] = shortcuts_list.get_iter_from_string(path);
-        if (!ok)
-            return;
-
-        this.settings.set_strv(
-            shortcuts_list.get_value(iter, COLUMN_SETTINGS_KEY),
-            accel_key ? [Gtk.accelerator_name(accel_key, accel_mods)] : []
-        );
-    }
-
-    reset() {
-        [this.shortcuts_list, this.global_shortcuts_list].forEach(shortcuts_list => {
-            shortcuts_list.foreach((model, path, iter) => {
-                const key = model.get_value(iter, COLUMN_SETTINGS_KEY);
-                this.settings.reset(key);
-            });
-        });
-    }
-
-    remove_shortcut(shortcuts_list, accel_key, accel_mods) {
-        shortcuts_list.foreach((model, path, iter) => {
-            const settings_key = model.get_value(iter, COLUMN_SETTINGS_KEY);
-            const value = this.settings.get_strv(settings_key);
-            const index = value.findIndex(accel => {
-                const [key, mods] = accelerator_parse(accel);
-                return key === accel_key && mods === accel_mods;
-            });
-
-            if (index !== -1) {
-                value.splice(index, 1);
-                this.settings.set_strv(settings_key, value);
-            }
-        });
     }
 
     grab_global_keys(cell_renderer, editable) {
