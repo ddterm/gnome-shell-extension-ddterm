47fbaaf7fb3124488cb101fa56d6f98c38807b7e
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-02-26 01:53:49 +0200

    app/extensiondbus: log D-Bus proxy creation

--- ddterm/app/extensiondbus.js
+++ ddterm/app/extensiondbus.js
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import GObject from 'gi://GObject';
 import Gio from 'gi://Gio';
 
 import { get_resource_text } from './resources.js';
@@ -15,15 +16,20 @@ const DEFAULT_FLAGS =
     Gio.DBusProxyFlags.GET_INVALIDATED_PROPERTIES |
     Gio.DBusProxyFlags.DO_NOT_CONNECT_SIGNALS;
 
+const BUS_NAME = 'org.gnome.Shell';
+const OBJECT_PATH = '/org/gnome/Shell/Extensions/ddterm';
+
 export function create_extension_dbus_proxy(connection = null, flags = DEFAULT_FLAGS) {
-    return proxy_factory(
-        connection ?? Gio.DBus.session,
-        'org.gnome.Shell',
-        '/org/gnome/Shell/Extensions/ddterm',
-        undefined,
-        undefined,
-        flags
-    );
+    const flags_str = GObject.flags_to_string(Gio.DBusProxyFlags, flags);
+    connection = connection ?? Gio.DBus.session;
+
+    console.debug('Connecting to %O %O with flags %O', BUS_NAME, OBJECT_PATH, flags_str);
+
+    const proxy = proxy_factory(connection, BUS_NAME, OBJECT_PATH, undefined, undefined, flags);
+
+    console.debug('Connected to %O %O with flags %O', BUS_NAME, OBJECT_PATH, flags_str);
+
+    return proxy;
 }
 
 export function create_extension_dbus_proxy_oneshot() {
