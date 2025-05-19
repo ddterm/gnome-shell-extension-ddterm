58cfe833954e65cf426fff7b2ea688030e69d34f
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-05-19 04:56:50 +0300

    ci: run on pr and push to master, fine-grained actions

--- .github/bin/faketty.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# SPDX-FileCopyrightText: 2024 Aleksandr Mezin <mezin.alexander@gmail.com>
-#
-# SPDX-License-Identifier: CC0-1.0
-
-exec script -q -e -c "stty cols 80; $(printf "%q " "$@")"
