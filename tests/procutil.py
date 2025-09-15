74925ac5366afbda902e16bd50603b09fc0e31dc
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-09-15 19:37:33 +0300

    tests/procutil: avoid `UnboundLocalError` if `POpen` constructor raises

--- tests/procutil.py
+++ tests/procutil.py
@@ -84,6 +84,7 @@ class Launcher:
 
         args, kwargs = self.tweak(args, kwargs)
         cmdline = shlex.join(args)
+        popen = None
 
         LOGGER.info('Starting process: %r', cmdline)
 
@@ -122,25 +123,26 @@ class Launcher:
                         raise
 
         finally:
-            if popen.returncode is None:
-                LOGGER.info('Process %r %r is still running', popen.pid, cmdline)
+            if popen is not None:
+                if popen.returncode is None:
+                    LOGGER.info('Process %r %r is still running', popen.pid, cmdline)
 
-            elif popen.returncode < 0:
-                LOGGER.info(
-                    'Process %r %r terminated by signal %s %s',
-                    popen.pid,
-                    cmdline,
-                    -popen.returncode,
-                    signal.strsignal(-popen.returncode),
-                )
+                elif popen.returncode < 0:
+                    LOGGER.info(
+                        'Process %r %r terminated by signal %s %s',
+                        popen.pid,
+                        cmdline,
+                        -popen.returncode,
+                        signal.strsignal(-popen.returncode),
+                    )
 
-            else:
-                LOGGER.info(
-                    'Process %r %r exited with code %s',
-                    popen.pid,
-                    cmdline,
-                    popen.returncode
-                )
+                else:
+                    LOGGER.info(
+                        'Process %r %r exited with code %s',
+                        popen.pid,
+                        cmdline,
+                        popen.returncode
+                    )
 
 
 class ContainerExecLauncher(Launcher):
